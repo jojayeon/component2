93889f6374fccc12963515c0c28115f0959f18c4
jojayeon <jojayeon6152@gmail.com>
2024-06-12 11:57:37 +0900

    생성자 함수

--- work-2.js
+++ work-2.js
@@ -2,15 +2,26 @@ const component = () => {
   let obj = {
     name : "조자연",
     age : 26,
-    job : "ㅇㅇ!!!생각이 너무 많아서 탈이다"
+    job : "ㅇㅇ"
   };
   
   let innerFunc = () => {
-
+    if(obj.age > 20) {
+      return "여름엔 맥주지!!!!"
+    } else {
+      return "여름엔 오구르트지!!!!"
+    }
   }
-
+  let auotherFunc = () => {
+    if(obj.job === "ㅇㅇ") {
+      return "상태가 메롱이군"
+    }else {
+      return "상태가 좋군"
+    }
+  }
   return `
   <h1>${dbj.name}</h1>
-
+  <h2>${innerFunc()}</h2>
+  <h3>${auotherFunc()}</h3>
   `;
 }
