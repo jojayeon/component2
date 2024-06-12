4feb45b0a37c036f50112430033804bfb1ac9ba8
jojayeon <jojayeon6152@gmail.com>
2024-06-12 11:48:40 +0900

    정적으로 만듬

--- work-2.js
+++ work-2.js
@@ -1,9 +1,16 @@
 const component = () => {
-  let obj = {};
+  let obj = {
+    name : "조자연",
+    age : 26,
+    job : "ㅇㅇ!!!생각이 너무 많아서 탈이다"
+  };
   
   let innerFunc = () => {
 
   }
 
-  return "some string";
+  return `
+  <h1>${dbj.name}</h1>
+
+  `;
 }
