506e8fdd1caab0628e2781a5b23a7b7514627cd9
jojayeon <jojayeon6152@gmail.com>
2024-06-12 10:44:48 +0900

    work-1작업

--- work-1.js
+++ work-1.js
@@ -1,8 +1,14 @@
-const basicData = {
-  name : "",
-  age: 0,
-  job : ""
+const basicData = () => {
+  return {
+    name : "조자연",
+    age: 26,
+    job : "버스에서 2.5시간 버티기"
+  }
 }
  const component = () => {
-  
- }
+  let obj = basicData();
+
+  const decision = () => {};
+
+  return `<html></html>`;
+}
