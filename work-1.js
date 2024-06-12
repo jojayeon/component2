1d6434479e091aff5daef87130b73e18557d25e4
jojayeon <jojayeon6152@gmail.com>
2024-06-12 11:42:46 +0900

    work-1작업중, work-1-1 강사님이 주신 코드

--- work-1.js
+++ work-1.js
@@ -1,12 +1,56 @@
-const basicData = () => {
-  return {
-    name : "조자연",
-    age: 26,
-    job : "버스에서 2.5시간 버티기"
+//1. 생성자 함수 setter로 규격만들기
+//2. 생성자 함수로 객체 생성하기
+//3. 생성자 함수 출러르 검사한기
+//instanceof
+// 4. true -> 문자열 html 리턴
+// 5. throw new error()
+
+
+class BasicData {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(vlaue) {
+    if(typeof(vlaue) === "string") {    
+      
+    }else {
+
+    }
+  }
+  
+  get age() {
+    return this._age;
+  }
+  set age(nage) {
+    if(typeof(nage) === "number") {
+      
+    }else {
+
+    }
+    
+  }
+  
+  get job() {
+    return this._job;
+  }
+  set job(vlaue) {
+    if(typeof(vlaue) === "string") {    
+      
+    }else {
+      throw new ;
+    }
   }
 }
+
+
  const component = () => {
-  let obj = basicData();
+  let obj = new basicData("조자연",26, "학생");
 
   const decision = () => {};
 
