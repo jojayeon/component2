fad03a962bf1aa62875dbffc78728e05cd68eb27
jojayeon <jojayeon6152@gmail.com>
2024-06-12 16:47:37 +0900

    date의 시 찾기 테스트

--- weamplate.js
+++ weamplate.js
@@ -3,19 +3,36 @@ const tagComponent = (tagName, textNode) => {
 }
 
 const container = () => {
-  
+  let today = new Date();
+
+// let ntoday = today.s
+
+
+// let day = () => {
+  //   if() {}
+  // }
   
   return `
-    ${tagComponent('header', '헤더부분')}
-    ${tagComponent('main', '메인부분')}
-    ${tagComponent('footer', '푸터부분')}
+  ${tagComponent('header', '헤더부분')}
+  ${tagComponent('main', '메인부분')}
+  ${tagComponent('footer', '푸터부분')}
   `;
 }
 let today = new Date();
+// let tod = today.splice()
+// let tod1 = today.slice("t")
+// let tod = today.split("t")
 console.log(today);
+// console.log(tod);
+// console.log(tod1);
+console.log(typeof(today));
+console.log(today.getHours());
+console.log(today.getMinutes());
+
+
 // 2024-06-12T03:25:51.
 //           t 03 = 이게 12시임 영국 기준이라 그런듯
-//여기서 
+ 
 
 //오늘은 6.12(날짜)or(오전 오후로 구분해도 좋음) 아니라면 메인부분 빼기 
 // 맞으면 오늘은 잘살고 있습니다.
